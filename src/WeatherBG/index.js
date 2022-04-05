d5ceeafa3f3786b47c5d610430078cd6ab9381cd
Diptabose <83805205+Diptabose@users.noreply.github.com>
2022-04-05 16:09:54 +0530

    Add files via upload

--- src/WeatherBG/index.js
+++ src/WeatherBG/index.js
@@ -1,1 +1,11 @@
+import foggy from './foggy.jpeg';
+import night from './night.jpg';
+import night1 from './night1.jpg';
+import night2 from './night2.jpg';
+import rainy from './rainy.jpeg';
+import sunrise from './sunrise.jpeg';
+import thunder from './thunder2.jpeg';
 
+const weatherbg= {foggy,night ,night1, night2, rainy , sunrise , thunder};
+
+export default weatherbg;
