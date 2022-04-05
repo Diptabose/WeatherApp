d0cc5153003a9883ce90c87ccba3cf3d446721ef
Diptabose <83805205+Diptabose@users.noreply.github.com>
2022-04-05 16:01:39 +0530

    Add files via upload

--- src/Theme.js
+++ src/Theme.js
@@ -1,1 +1,27 @@
+export const lightTheme={
+ bgcolor:'bg-gray-200',
+//  bgcolor:'bg-sky-300',
+  textcolor:'text-black',
+  bordercolor:'border-black',
+  plotDataColor:'black',
+  plotxColor:'#dd264c',
+  boxshadow:'shadow-black',
+  invert:'invert',
+  detailsColor:'bg-slate-400',
+  sevendayCardColor:'bg-gray-400',
+}
+
+export const darkTheme={
+  bgcolor:'bg-black',
+  //bgcolor:'bg-sky-800',
+  textcolor:'text-gray-100',
+  bordercolor:'border-gray-100',
+  plotDataColor:'white',
+  plotxColor:'#dd264c',
+  boxshadow:'shadow-white',
+  invert:'invert-0',
+  detailsColor:'bg-slate-900',
+  sevendayCardColor:'bg-slate-700',
+}
+
 
