8040c9478b599ceb481bc04b5008797401e16112
Diptabose <83805205+Diptabose@users.noreply.github.com>
2022-06-02 12:26:21 +0530

    Add files via upload

--- src/service-worker.js
+++ src/service-worker.js
@@ -76,7 +76,7 @@ self.addEventListener('install',(e)=>{
   self.skipWaiting();
 });
 self.addEventListener('activate',(e)=>{
-  self.clients.claim().then(()=>{console.log('controlling worker on all pages');});
+  self.clients.claim().then(()=>{});
 });
 self.addEventListener("push", e => {
     const data = e.data.json();
